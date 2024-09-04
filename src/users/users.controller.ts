5e02cdee099a33ef18217cd3adc06ab5cfed7177
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-09-04 14:11:06 +0100

    custom data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,5 @@
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from './../interceptors/serialize.interceptor';
 import {
   Body,
   Controller,
@@ -8,14 +10,13 @@ import {
   Query,
   Delete,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -24,7 +25,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
@@ -32,6 +32,8 @@ export class UsersController {
     if (!user) {
       throw new NotFoundException('user not found');
     }
+
+    return user;
   }
 
   @Get('')

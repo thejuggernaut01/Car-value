1fed70e967d6aeeba514e0f7575f507c1a58ccba
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-09-08 17:33:03 +0100

    validate and transform query string values

---
+++ src/reports/dtos/get-estimate.dto.ts
@@ -0,0 +1,43 @@
+import { Transform } from 'class-transformer';
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class GetEstimateDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  // Value is going to be the incoming year in string form.
+  // we then parse it into an integer and then return it from
+  // the function. the parse integer will then be assigned to
+  // the year property
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  // We use parseFloat because our lng and lat can
+  // be a number with decimal on it
+  @Transform(({ value }) => parseFloat(value))
+  @IsLongitude()
+  lng: number;
+
+  @Transform(({ value }) => parseFloat(value))
+  @IsLatitude()
+  lat: number;
+}

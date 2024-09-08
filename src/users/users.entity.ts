9ab489bd1322a819d1de8486159c1a2a7a53958d
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-09-08 02:54:22 +0100

    report creation, building associations

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,3 +1,4 @@
+import { Report } from '../reports/report.entity';
 import {
   Entity,
   Column,
@@ -5,6 +6,7 @@ import {
   AfterInsert,
   AfterUpdate,
   AfterRemove,
+  OneToMany,
 } from 'typeorm';
 
 @Entity()
@@ -17,6 +19,24 @@ export class User {
 
   @Column()
   password: string;
+
+  // The function call in the first argument is to solve
+  // circular dependency issue
+
+  // The second argument is very specialized. THe use case of is
+  // particular to how typeORM internally models relationship
+  // between different entities and does validation of how all
+  // these relationships are set up.
+
+  // The second argument becomes more necessary; it's when we got
+  // two different entity that are related in more than one way.
+
+  // It takes an instance of the entity we're trying to relate to
+  // and then return how to go from that target entity back to the
+  // entity that we are currently defining, which is the user.
+  // In the report entity, it's the opposite.
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
